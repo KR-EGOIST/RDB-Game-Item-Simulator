578c1ad428d5aac1bc862ab25e32515f58bfd40a
윤진호 <jhso5041@naver.com>
2024-05-22 21:39:15 +0900

    🎉 add : 회원가입 API, 로그인 API 추가 , 미들웨어 추가

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+import UsersRouter from './routes/users.router.js';
+
+const app = express();
+const PORT = 3018;
+
+app.use(LogMiddleware);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api', [UsersRouter]);
+
+app.use(ErrorHandlingMiddleware);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
