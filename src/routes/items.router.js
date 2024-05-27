67f8a56c77a8dc860fc8554b99d9e51359125531
윤진호 <jhso5041@naver.com>
2024-05-27 12:21:01 +0900

    ✨ update : 이미 장착한 아이템을 수정할 경우 변경된 수치를 장착한 캐릭터에게 적용

--- src/routes/items.router.js
+++ src/routes/items.router.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { userPrisma } from '../utils/prisma/userClient.js';
 import { itemPrisma } from '../utils/prisma/itemClient.js';
+import { Container } from 'winston';
 
 const router = express.Router();
 
@@ -72,7 +74,7 @@ router.post('/items', async (req, res, next) => {
 
 /* 아이템 수정 API */
 router.patch('/items/:itemcode', async (req, res, next) => {
-  const { itemCode: itemcode } = req.params;
+  const { itemcode } = req.params;
   const { item_name, item_stat } = req.body;
 
   const item = await itemPrisma.items.findFirst({
@@ -84,6 +86,51 @@ router.patch('/items/:itemcode', async (req, res, next) => {
   if (!item) {
     return res.status(404).json({
       message: '존재하지 않는 아이템입니다.',
+    });
+  }
+
+  // 수정할려는 아이템을 이미 장착한 캐릭터의 health, power 수치 변경
+  const findItem = await itemPrisma.itemStats.findFirst({
+    where: {
+      ItemCode: +itemcode,
+    },
+  });
+
+  const changeHealth =
+    findItem.health > item_stat.health
+      ? findItem.health - item_stat.health
+      : item_stat.health - findItem.health;
+
+  const changePower =
+    findItem.power > item_stat.power
+      ? findItem.power - item_stat.power
+      : item_stat.power - findItem.power;
+
+  const isEquip = await userPrisma.inventorys.findMany({
+    where: {
+      ItemCode: item.itemCode,
+    },
+    select: {
+      CharacterId: true,
+    },
+  });
+
+  const findCharacter = isEquip.map(({ CharacterId }) => CharacterId);
+  for (const id of findCharacter) {
+    const character = await userPrisma.characters.findFirst({
+      where: {
+        characterId: id,
+      },
+    });
+
+    await userPrisma.characters.update({
+      where: {
+        characterId: character.characterId,
+      },
+      data: {
+        health: character.health - changeHealth,
+        power: character.power - changePower,
+      },
     });
   }
 
@@ -144,7 +191,7 @@ router.get('/items', async (req, res, next) => {
 
 /* 아이템 상세 조회 API */
 router.get('/items/:itemcode', async (req, res, next) => {
-  const { itemCode: itemcode } = req.params;
+  const { itemcode } = req.params;
 
   const item = await itemPrisma.items.findFirst({
     where: {
