b1bf83efde6f829cbc8ebc351d21aef296491af5
윤진호 <jhso5041@naver.com>
2024-05-28 11:33:38 +0900

    🐛 fix : EC2 PrismaClient 인식 안되는 버그 (테스트)

--- src/utils/prisma/userClient.js
+++ src/utils/prisma/userClient.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../../prisma/userClient/default.js';
+import { PrismaClient } from '../../../prisma/userClient/index.js';
 
 export const userPrisma = new PrismaClient({
   // Prisma를 이용해 데이터베이스를 접근할 때, SQL을 출력해줍니다.
